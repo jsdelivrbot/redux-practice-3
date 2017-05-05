4ddb953e393dc7b2b0d03576ca2edb48209f42fb
Jeong Min Lee <jacklee0223@gmail.com>
2017-05-05 15:36:42 -0700

    show book pages

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
 	return [
-		{ title: 'Javascript: The Good Parts' },
-		{ title: 'Harry Potter' },
-		{ title: 'The Dark Tower' },
-		{ title: 'Eloquent Ruby' },
+		{ title: 'Javascript: The Good Parts', pages: 38 },
+		{ title: 'Harry Potter', pages: 18 },
+		{ title: 'The Dark Tower', pages: 41 },
+		{ title: 'Eloquent Ruby', pages: 25 },
 	]
 }
